4698c6788665f76a49a21283ed0eef6f27d83af5
Towobola <towobolak@gmail.com>
2024-03-22 15:51:54 +0100

    final

--- count.js
+++ count.js
@@ -1,63 +1,26 @@
-function submit(){
 
-var dayss = document.getElementById("dayss").val();
-var hourss = document.getElementById("hourss").val();
-var minutess = document.getElementById("minutess").val();
-var secondss = document.getElementById("secondss").val(); 
-var date = document.querySelectorAll('#dayss, #hourss, #minutess, #secondss');
+const dayss = document.getElementById("daysss");
+const hourss = document.getElementById("hoursss");
+const minutess = document.getElementById("minutesss");
+const secondss = document.getElementById("secondsss"); 
 
-document.getElementById("dayss").style.display = "none";
-document.getElementById("hourss").style.display = "none";
-document.getElementById("minutess").style.display = "none";
-document.getElementById("secondss").style.display = "none";
+const currentYear = new Date().getTime();
 
-document.getElementById("daysss").style.display = "block";
-document.getElementById("hoursss").style.display = "block";
-document.getElementById("minutesss").style.display = "block";
-document.getElementById("secondsss").style.display = "block";
+const countDownDate = new Date("March 30, 2024 23:59:59").getTime();
 
+function updateCountdowntime() {
+  const currentTime = new Date();
+  const distance = countDownDate - currentTime;
 
-var countDownDate = new Date("date").getTime();
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-var x = setInterval(function() {
-
- var now = new Date().getTime();
-    
-  var distance = countDownDate - now;
-    
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  document.getElementById("daysss").innerHTML = days ;
-
-  document.getElementById("hoursss").innerHTML = hours ;
-
-  document.getElementById("minutesss").innerHTML = minutes ;
-
-  document.getElementById("secondsss").innerHTML = seconds ;
-    
-  if (distance < 0) {
-    clearInterval(x);
-
-    document.getElementById("daysss").innerHTML = "0" ;
-
-    document.getElementById("hoursss").innerHTML = "0" ;
-  
-    document.getElementById("minutesss").innerHTML = "0" ;
-  
-    document.getElementById("secondsss").innerHTML = "0" ;
-  }
-
-}, 1000);
-
-
-
-
-
-
-
+  dayss.innerHTML = days;
+  hourss.innerHTML = hours;
+  minutess.innerHTML = minutes;
+  secondss.innerHTML = seconds;
 }
 
-
+setInterval(updateCountdowntime, 1000);
